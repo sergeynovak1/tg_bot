a70de7e636a880add1ce01479de30be1725d8e41
Sergey <ssnovak@stud.kpfu.ru>
2023-01-21 17:33:38 +0300

    winning bot strategies

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -89,41 +89,50 @@ def bot_move():
 
     if move_list:
         if (4 in move_list) and ((0 in move_list_o and 8 in move_list_o) or (2 in move_list_o and 6 in move_list_o) or
-                                  (1 in move_list_o and 7 in move_list_o) or (3 in move_list_o and 5 in move_list_o) or
-                                 (0 in move_list_x and 8 in move_list_x) or (2 in move_list_x and 6 in move_list_x) or
-                                  (1 in move_list_x and 7 in move_list_x) or (3 in move_list_x and 5 in move_list_x)):
+                                  (1 in move_list_o and 7 in move_list_o) or (3 in move_list_o and 5 in move_list_o)):
             field[4] = O_SIMBOL
         elif (2 in move_list) and ((0 in move_list_o and 1 in move_list_o) or (5 in move_list_o and 8 in move_list_o) or
-                                    (4 in move_list_o and 6 in move_list_o) or
-                                   (0 in move_list_x and 1 in move_list_x) or (5 in move_list_x and 8 in move_list_x) or
-                                    (4 in move_list_x and 6 in move_list_x)):
+                                    (4 in move_list_o and 6 in move_list_o)):
             field[2] = O_SIMBOL
         elif (0 in move_list) and ((1 in move_list_o and 2 in move_list_o) or (3 in move_list_o and 6 in move_list_o) or
-                                    (4 in move_list_o and 8 in move_list_o) or
-                                   (1 in move_list_x and 2 in move_list_x) or (3 in move_list_x and 6 in move_list_x) or
-                                    (4 in move_list_x and 8 in move_list_x)):
+                                    (4 in move_list_o and 8 in move_list_o)):
             field[0] = O_SIMBOL
         elif (8 in move_list) and ((0 in move_list_o and 4 in move_list_o) or (2 in move_list_o and 5 in move_list_o) or
-                                    (6 in move_list_o and 7 in move_list_o) or
-                                   (0 in move_list_x and 4 in move_list_x) or (2 in move_list_x and 5 in move_list_x) or
-                                    (6 in move_list_x and 7 in move_list_x)):
+                                    (6 in move_list_o and 7 in move_list_o)):
             field[8] = O_SIMBOL
         elif (6 in move_list) and ((0 in move_list_o and 3 in move_list_o) or (2 in move_list_o and 4 in move_list_o) or
-                                    (7 in move_list_o and 8 in move_list_o) or
-                                   (0 in move_list_x and 3 in move_list_x) or (2 in move_list_x and 4 in move_list_x) or
+                                    (7 in move_list_o and 8 in move_list_o)):
+            field[6] = O_SIMBOL
+        elif (1 in move_list) and ((0 in move_list_o and 2 in move_list_o) or (4 in move_list_o and 7 in move_list_o)):
+            field[1] = O_SIMBOL
+        elif (3 in move_list) and ((0 in move_list_o and 6 in move_list_o) or (4 in move_list_o and 5 in move_list_o)):
+            field[3] = O_SIMBOL
+        elif (5 in move_list) and ((3 in move_list_o and 4 in move_list_o) or (2 in move_list_o and 8 in move_list_o)):
+            field[5] = O_SIMBOL
+        elif (7 in move_list) and ((1 in move_list_o and 4 in move_list_o) or (6 in move_list_o and 8 in move_list_o)):
+            field[7] = O_SIMBOL
+        elif (4 in move_list) and ((0 in move_list_x and 8 in move_list_x) or (2 in move_list_x and 6 in move_list_x) or
+                                  (1 in move_list_x and 7 in move_list_x) or (3 in move_list_x and 5 in move_list_x)):
+            field[4] = O_SIMBOL
+        elif (2 in move_list) and ((0 in move_list_x and 1 in move_list_x) or (5 in move_list_x and 8 in move_list_x) or
+                                    (4 in move_list_x and 6 in move_list_x)):
+            field[2] = O_SIMBOL
+        elif (0 in move_list) and ((1 in move_list_x and 2 in move_list_x) or (3 in move_list_x and 6 in move_list_x) or
+                                    (4 in move_list_x and 8 in move_list_x)):
+            field[0] = O_SIMBOL
+        elif (8 in move_list) and ((0 in move_list_x and 4 in move_list_x) or (2 in move_list_x and 5 in move_list_x) or
+                                    (6 in move_list_x and 7 in move_list_x)):
+            field[8] = O_SIMBOL
+        elif (6 in move_list) and ((0 in move_list_x and 3 in move_list_x) or (2 in move_list_x and 4 in move_list_x) or
                                     (7 in move_list_x and 8 in move_list_x)):
             field[6] = O_SIMBOL
-        elif (1 in move_list) and ((0 in move_list_o and 2 in move_list_o) or (4 in move_list_o and 7 in move_list_o) or
-                                   (0 in move_list_x and 2 in move_list_x) or (4 in move_list_x and 7 in move_list_x)):
+        elif (1 in move_list) and ((0 in move_list_x and 2 in move_list_x) or (4 in move_list_x and 7 in move_list_x)):
             field[1] = O_SIMBOL
-        elif (3 in move_list) and ((0 in move_list_o and 6 in move_list_o) or (4 in move_list_o and 5 in move_list_o) or
-                                   (0 in move_list_x and 6 in move_list_x) or (4 in move_list_x and 5 in move_list_x)):
+        elif (3 in move_list) and ((0 in move_list_x and 6 in move_list_x) or (4 in move_list_x and 5 in move_list_x)):
             field[3] = O_SIMBOL
-        elif (5 in move_list) and ((3 in move_list_o and 4 in move_list_o) or (2 in move_list_o and 8 in move_list_o) or
-                                   (3 in move_list_x and 4 in move_list_x) or (2 in move_list_x and 8 in move_list_x)):
+        elif (5 in move_list) and ((3 in move_list_x and 4 in move_list_x) or (2 in move_list_x and 8 in move_list_x)):
             field[5] = O_SIMBOL
-        elif (7 in move_list) and ((1 in move_list_o and 4 in move_list_o) or (6 in move_list_o and 8 in move_list_o) or
-                                   (1 in move_list_x and 4 in move_list_x) or (6 in move_list_x and 8 in move_list_x)):
+        elif (7 in move_list) and ((1 in move_list_x and 4 in move_list_x) or (6 in move_list_x and 8 in move_list_x)):
             field[7] = O_SIMBOL
         else:
             field[random.choice(move_list)] = O_SIMBOL
